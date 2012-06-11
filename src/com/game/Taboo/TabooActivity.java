679eeef91d168addbf94f48dd83f6cb78bc1cbf5
Neha Sinha <neha.sinha@sv.cmu.edu>
2012-06-11 04:15:37 -0700

    first screen and touch screen position working

---
+++ src/com/game/Taboo/TabooActivity.java
@@ -0,0 +1,33 @@
+package com.game.Taboo;
+
+
+import android.os.Bundle;
+import android.app.ListActivity; 
+import android.content.Intent; 
+import android.view.View;
+import android.widget.ArrayAdapter; 
+import android.widget.ListView;
+
+public class TabooActivity extends ListActivity {
+	String options[] = {"NewGame", "Resume Game", "Stop Game" };
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+    	super.onCreate(savedInstanceState);
+        setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, options));
+    }
+    
+    @Override
+    protected void onListItemClick(ListView list, View view, int position, long id) {
+    	super.onListItemClick(list, view, position, id); 
+    	String testName = options[position];
+    	try {
+    			Class clazz = Class.forName("com.game.Taboo." + testName);
+    			Intent intent = new Intent(this, clazz);
+    			startActivity(intent);
+    	} catch (ClassNotFoundException e) {
+    		e.printStackTrace(); 
+    	}
+    } 
+ }
